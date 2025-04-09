a66d04054cca6714feeb17c4370d53edfbc13811
Yash Patel <yashbpatel2004@gmail.com>
2025-04-09 09:22:27 -0400

    minor fixes

--- frontend/src/route/routes.jsx
+++ frontend/src/route/routes.jsx
@@ -26,7 +26,7 @@ const router = createBrowserRouter([
       { path: "dashboard", element: <Dashboard />, errorElement: <ErrorDisplay /> },
       { path: "profile", element: <UserProfile /> },
       { path: "friends-list", element: <Friends /> },
-      { path: "profile/edit", element: <EditProfile /> },
+      { path: "edit-profile", element: <EditProfile /> },
       { path: "friends", element: <Friends /> },
       { path: "forgot", element: <ForgotPassword/>},
       { path: "accountsettings", element: <AccountSettings /> }
